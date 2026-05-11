f3ee33e32c36bb7e6c3cf727d1f647d623ee5a50
SWAFWANA SHERI <swafwanasheri17@gmail.com>
2026-05-11 20:40:10 +0530

    Chat with ui connected

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request
-
+from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 from groq import Groq
 from dotenv import load_dotenv
@@ -8,14 +8,15 @@ import os
 load_dotenv()
 
 app = FastAPI()
-
-
+templates = Jinja2Templates(directory="templates")
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 class ChatInput(BaseModel):
     message: str
 
-client = Groq(api_key=os.getenv("GROQ_API_KEY"))
-
+@app.get("/")
+async def home(request: Request):
+    return templates.TemplateResponse(request, "index.html")
 
 @app.post("/chat")
 def chat(data: ChatInput):
@@ -23,4 +24,4 @@ def chat(data: ChatInput):
         model="llama-3.3-70b-versatile",
         messages=[{"role": "user", "content": data.message}]
     )
-    return {"reply": response.choices[0].message.content}
+    return {"response": response.choices[0].message.content}  # changed "reply" → "response"
